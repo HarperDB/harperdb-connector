d57834ea48562b1b37218c0e3ecf4dec2de325c4
kyle <kyle@harperdb.io>
2018-06-22 11:15:59 -0600

    Built out the functions and added basic code comments

--- code/libraries/HarperDB/HarperDB.js
+++ code/libraries/HarperDB/HarperDB.js
@@ -15,42 +15,155 @@ function HarperDB(username, password, end_point){
      * @param {string} schema
      */
     function createSchema(schema, callback){
+        let body = {
+            operation:"create_schema",
+            schema: schema
+        };
 
+        executeRequest(body, callback);
     }
 
+    /**
+     * Create a table
+     * @param schema
+     * @param table
+     * @param callback
+     */
     function createTable(schema, table, callback){
+        let body = {
+            operation:"create_table",
+            schema: schema,
+            table: table
+        };
 
+        executeRequest(body, callback);
     }
 
+    /**
+     * Insert record(s)
+     * @param schema
+     * @param table
+     * @param records
+     * @param callback
+     */
     function insert(schema, table, records, callback){
+        let body = {
+            operation:"insert",
+            schema: schema,
+            table: table,
+            records:records
+        };
 
+        executeRequest(body, callback);
     }
 
+    /**
+     * Update record(s)
+     * @param schema
+     * @param table
+     * @param records
+     * @param callback
+     */
     function update(schema, table, records, callback){
+        let body = {
+            operation:"update",
+            schema: schema,
+            table: table,
+            records:records
+        };
 
+        executeRequest(body, callback);
     }
 
+    /**
+     * Delete record(s)
+     * @param schema
+     * @param table
+     * @param ids
+     * @param callback
+     */
     function deleter(schema, table, ids, callback){
+        let body = {
+            operation:"delete",
+            schema: schema,
+            table: table,
+            hash_values:ids
+        };
 
+        executeRequest(body, callback);
     }
 
+    /**
+     * Search for records based on hash (primary key)
+     * @param schema
+     * @param table
+     * @param ids
+     * @param attributes
+     * @param callback
+     */
     function searchByHash(schema, table, ids, attributes, callback){
+        let body = {
+            operation:"search_by_hash",
+            schema: schema,
+            table: table,
+            hash_values:ids
+            get_attributes: attributes ? attributes : ["*"]
+        };
 
+        executeRequest(body, callback);
     }
 
-    function searchByValue(schema, table, value, attributes, callback){
+    /**
+     * Search by value on an attribute
+     * @param schema
+     * @param table
+     * @param search_attribute
+     * @param search_value
+     * @param attributes
+     * @param callback
+     */
+    function searchByValue(schema, table, search_attribute, search_value, attributes, callback){
+        let body = {
+            operation:"search_by_value",
+            schema: schema,
+            table: table,
+            search_attribute: search_attribute,
+            search_value: search_value,
+            get_attributes: attributes ? attributes : ["*"]
+        };
 
+        executeRequest(body, callback);
     }
 
+    /**
+     * Run SQL (INSERT / UPDATE / DELETE / SELECT)
+     * @param sql
+     * @param callback
+     */
     function sql(sql, callback){
+        let body = {
+            operation:"sql",
+            sql: sql
+        };
 
+        executeRequest(body, callback);
     }
 
+    /**
+     * Execute a raw HarperDB call
+     * @param json
+     * @param callback
+     */
     function executeOperation(json, callback){
-
+        executeRequest(json, callback);
     }
 
-    function buildRequest(body){
+    /**
+     * Perform the request to HarperDB
+     * @param body
+     * @param callback
+     */
+    function executeRequest(body, callback){
         var options = {
             uri:end_point,
             body: body,
@@ -62,6 +175,14 @@ function HarperDB(username, password, end_point){
                 pass: password
             }
         };
+
+        http.post(options, (err, results)=>{
+            if(err){
+                return callback(err);
+            }
+
+            callback(null, results);
+        });
     }
 
     return {
